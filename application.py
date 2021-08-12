123b3fc03e5d97e11092348d45ae783e2d57bbc1
Pablo <pdiaz2@uc.cl>
2021-08-12 09:20:40 -0400

    Changed app for application in application.py

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 from flaskblog import create_app
 
-app = create_app()
+application = create_app()
 # To run through python the thing below
 if __name__ == "__main__":
-    app.run(debug=True)
+    application.run(debug=True)
